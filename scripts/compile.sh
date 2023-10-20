96d5d581abd578ec138a35ec984d633c5a9f5f3a
Ramon Fraga Pereira <ramonfpereira@gmail.com>
2023-10-20 18:41:55 +0100

    Update compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,7 @@
 echo 'Compiling the main project...'
-g++ -Wall -O3 -std=c++17  src/main.cpp -o main.bin
 
+# Command for compiling the main project in Linux
+# g++ -Wall -O3 -std=c++17  src/main.cpp -o main.bin
+
+# Command for compiling the main project in macOS
+/opt/homebrew/bin/g++-13 -Wall -O3 -std=c++17 -ld_classic src/main.cpp -o main.bin
